9289c3882a92079b701dfc8ab704543287888435
Lewis <marshy2201@yahoo.co.uk>
2020-10-05 13:54:38 +0100

    navbar made responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,16 @@ import { Navbar, Nav } from 'react-bootstrap';
 import LewisImage from '../images/lewis.jpg';
 
 const NavbarComponent = () => (
-  <Navbar fixed="top" expand="lg">
-    <Navbar.Collapse className="d-flex flex-column">
-      <Navbar.Brand href="#">
-        <span className="d-block lewis-image-wrapper p-2">
-          <img src={LewisImage} id="lewis-image" />
-        </span>
-      </Navbar.Brand>
-      <Nav className="d-flex flex-column text-center mt-3">
+  <Navbar collapseOnSelect variant="dark" fixed="top" expand="lg">
+    <Navbar.Brand href="#about" className="mr-0">
+      <span className="d-block d-lg-none">Lewis Marshall</span>
+      <span className="d-none d-lg-block lewis-image-wrapper p-2">
+        <img src={LewisImage} id="lewis-image" />
+      </span>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+    <Navbar.Collapse className="flex-grow-0">
+      <Nav defaultActiveKey="#about" className="d-flex flex-column text-center mt-3">
         <Nav.Link href="#about">About</Nav.Link>
         <Nav.Link href="#experience">Experience</Nav.Link>
         <Nav.Link href="#education">Education</Nav.Link>
