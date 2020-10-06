5ab6d9fa61931c969a3ff0d789eb3478655a72aa
Lewis <marshy2201@yahoo.co.uk>
2020-10-06 18:51:25 +0100

    typos corrected

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -9,10 +9,10 @@ const Experience = () => (
       company="Luxinar"
       about="Developer for a Laser manufacturing company where my role was to develop a new version of an internal web application used all around the company."
       responsibilities={[
-        "Developed the new internal web application using ember.js, node.js &amp; MongoDB.",
+        "Developed the new internal web application using ember.js, node.js & MongoDB.",
         "Designed a web interface for the latest laser products displaying diagnostics information and was developed using jQuery.",
         "Showed presentational demos of new or improved applications to business users taking feedback.",
-        "Provided training for other developers with ember.js, node.js &amp; MongoDB.",
+        "Provided training for other developers with ember.js, node.js & MongoDB.",
         "Worked using the waterfall environment method."
       ]}
       from="July 2017"
