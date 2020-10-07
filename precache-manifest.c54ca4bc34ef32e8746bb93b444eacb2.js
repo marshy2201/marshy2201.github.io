5dd2e3ca131299944b4d50a90e4305ea6b656460
Lewis <marshy2201@yahoo.co.uk>
2020-10-07 16:37:04 +0100

    Updates

---
+++ precache-manifest.c54ca4bc34ef32e8746bb93b444eacb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3729fc7f25a528e67781bc5b6af6c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c76ea5f62cec3369f40",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6f6026887e6eec5108ac",
+    "url": "/static/css/main.4ef91416.chunk.css"
+  },
+  {
+    "revision": "8c76ea5f62cec3369f40",
+    "url": "/static/js/2.14cff9ad.chunk.js"
+  },
+  {
+    "revision": "fee3ef41109770b3c68abe9f8e3cdf99",
+    "url": "/static/js/2.14cff9ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f6026887e6eec5108ac",
+    "url": "/static/js/main.dcc40a05.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "d64f57dd82ae05bfa0d675dac07ee328",
+    "url": "/static/media/lewis.d64f57dd.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.b2d3428a331e1fe5e3f3c71de2bf2594.js"
+  "/precache-manifest.c54ca4bc34ef32e8746bb93b444eacb2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
