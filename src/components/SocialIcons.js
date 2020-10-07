cd7e9f08937e656f270376e9014e367fbe40a84f
Lewis <marshy2201@yahoo.co.uk>
2020-10-07 17:04:38 +0100

    facebook link icon removed

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import SocialIcon from './SocialIcon';
-import { faGithub, faLinkedinIn, faFacebookF } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 const SocialIcons = () => (
   <div className="social-icons mt-4">
     <SocialIcon link="https://github.com/marshy2201" icon={faGithub} />
     <SocialIcon link="https://www.linkedin.com/in/lewis-marshall-64a3391b8/" icon={faLinkedinIn} />
-    <SocialIcon link="https://www.facebook.com/marshy2201" icon={faFacebookF} />
   </div>
 )
 
