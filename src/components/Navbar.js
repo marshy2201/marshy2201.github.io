757a9cd8cad16a29b89f2c2a4c8738baf9bf1480
Lewis <marshy2201@yahoo.co.uk>
2020-10-07 12:54:17 +0100

    react-scroll addon added, links have animation on click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import LewisImage from '../images/lewis.jpg';
+import { Link, animateScroll as scroll } from "react-scroll";
 
-const NavbarComponent = () => (
-  <Navbar collapseOnSelect variant="dark" fixed="top" expand="lg">
-    <Navbar.Brand href="#about" className="mr-0">
-      <span className="d-block d-lg-none">Lewis Marshall</span>
-      <span className="d-none d-lg-block lewis-image-wrapper p-2">
-        <img src={LewisImage} id="lewis-image" alt="Lewis Marshall" />
-      </span>
-    </Navbar.Brand>
-    <Navbar.Toggle />
-    <Navbar.Collapse className="flex-grow-0">
-      <Nav defaultActiveKey="#about" className="d-flex flex-column text-center mt-3">
-        <Nav.Link href="#about">About</Nav.Link>
-        <Nav.Link href="#experience">Experience</Nav.Link>
-        <Nav.Link href="#education">Education</Nav.Link>
-        <Nav.Link href="#skills">Skills</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-)
+const NavbarComponent = () => {
+  // expanded state
+  const [expanded, setExpanded] = useState(false);
+
+  // set expanded to false
+  const closeNav = () => setExpanded(false);
+
+  // scroll to top function
+  const scrollToTop = () => scroll.scrollToTop();
+
+  return (
+    <Navbar 
+      variant="dark" 
+      fixed="top"
+      expanded={expanded}
+      onToggle={boolean => setExpanded(boolean)} 
+      expand="lg"
+    >
+      <Navbar.Brand href="#" onClick={scrollToTop} className="mr-0">
+        <span className="d-block d-lg-none">Lewis Marshall</span>
+        <span className="d-none d-lg-block lewis-image-wrapper p-2">
+          <img src={LewisImage} id="lewis-image" alt="Lewis Marshall" />
+        </span>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="flex-grow-0">
+        <Nav className="d-flex flex-column text-center mt-3">
+          <Link 
+            href=""
+            smooth='easeInOutQuart'
+            spy
+            to="about"
+            onClick={closeNav} 
+            className="nav-link"
+          >About</Link>
+          <Link 
+            href=""
+            smooth='easeInOutQuart'
+            spy
+            to="experience" 
+            onClick={closeNav}
+            className="nav-link"
+          >Experience</Link>
+          <Link
+            href="" 
+            smooth='easeInOutQuart'
+            spy
+            to="education" 
+            onClick={closeNav}
+            className="nav-link"
+          >Education</Link>
+          <Link 
+            href=""
+            smooth='easeInOutQuart'
+            spy
+            to="skills" 
+            onClick={closeNav}
+            className="nav-link"
+          >Skills</Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
 
 export default NavbarComponent;
